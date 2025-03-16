5dbbe42480625ca40b6b8d773e0ea9999fdba676
tkeatkaew <thanit@rmutl.ac.th>
2025-03-16 14:26:50 +0700

    agent noti updated

--- lab1-application-server/backend/endpoint-api/apiconfig.js
+++ lab1-application-server/backend/endpoint-api/apiconfig.js
@@ -1,11 +1,21 @@
 var apiconfig = {
     development: {
         serverKey: '1aaZ!ARgAQGuQzp00D5D000000.mOv2jmhXkfIsjgywpCIh7.HZpc6vED1LCbc90DTaVDJwdNqbTW5r4uZicv8AFfkOE1ialqnR8UN5.wnAgh090h',
-
+        parseServerAPI: "https://localhost:4000/api",
+        parseServerWS: "wss://localhost:4000",
+        masterKey: "wallboardapi",
+        clientKey: "wallboardapi",
+        javascriptKey: "wallboardapi",
+        appId: "wallboardapi",
     },
     production: {
         serverKey: '1aaZ!ARgAQGuQzp00D5D000000.mOv2jmhXkfIsjgywpCIh7.HZpc6vED1LCbc90DTaVDJwdNqbTW5r4uZicv8AFfkOE1ialqnR8UN5.wnAgh090h',
-
+        hosturl: "https://lab-parse-server.se-rmutl.net/api",
+        wsurl: "wss://lab-parse-server.se-rmutl.net",
+        masterKey: "wallboardapi",
+        clientKey: "wallboardapi",
+        javascriptKey: "wallboardapi",
+        appId: "wallboardapi",
     },
 
 };
